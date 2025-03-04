74d32a63175a9741130b830e42a32cd4eb493732
ved08 <vedvardhan.g@gmail.com>
2025-03-04 21:50:41 +0530

    added create tree ix

--- native/mplbubblegumnifs/src/lib.rs
+++ native/mplbubblegumnifs/src/lib.rs
@@ -1,28 +1,35 @@
-use mpl_bubblegum::{
-    accounts::TreeConfig, instructions::CreateTreeConfigBuilder, programs::MPL_BUBBLEGUM_ID,
-};
-
-use solana_program::pubkey::Pubkey;
-use solana_sdk::{signature::Keypair, signer::Signer, signers::Signers};
+use mpl_bubblegum::instructions::CreateTreeConfigBuilder;
+use solana_program::pubkey::pubkey;
+use solana_pubkey::pubkey as solanapubkey;
+use solana_sdk::{pubkey::Pubkey, signature::Keypair, signer::Signer, system_instruction};
 
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
     a + b
 }
 
-fn test() {
-    let merkle_tree = Keypair::new();
-    let merkle_pubkey = &Pubkey::new_from_array(merkle_tree.pubkey().to_bytes());
-    let mpl_bubblegum_id = Pubkey::from_str_const("BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY");
+#[rustler::nif]
+fn test(lamports: u64, space: u64, max_depth: usize, max_buffer: usize) {
     let payer = Keypair::new();
+    let mpl_bubblegum_id = pubkey!("BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY");
+    let merkle_tree = Keypair::new();
     let (tree_config, _) =
-        Pubkey::find_program_address(&[merkle_pubkey.as_array()], &mpl_bubblegum_id);
-    // let create_tree_ix = CreateTreeConfigBuilder {
-    //     tree_config,
-    //     merkle_tree,
-    //     payer,
-    //     tree_creator: todo!(),
-    // };
-}
+        Pubkey::find_program_address(&[merkle_tree.pubkey().as_array()], &mpl_bubblegum_id);
+    let create_merkle_ix = system_instruction::create_account(
+        &payer.pubkey(),
+        &merkle_tree.pubkey(),
+        lamports,
+        space,
+        &payer.pubkey(),
+    );
 
+    let create_config_ix = CreateTreeConfigBuilder::new()
+        .tree_config(tree_config.to_bytes().into())
+        .merkle_tree(merkle_tree.pubkey().to_bytes().into())
+        .payer(payer.pubkey().to_bytes().into())
+        .tree_creator(payer.pubkey().to_bytes().into())
+        .max_depth(max_depth as u32)
+        .max_buffer_size(max_buffer as u32)
+        .instruction();
+}
 rustler::init!("Elixir.MplBubblegumNifs");
