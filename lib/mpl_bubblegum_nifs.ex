44d4b7545cba4b4220e09b2c62fc7c351cfad07c
ved08 <vedvardhan.g@gmail.com>
2025-03-07 00:39:03 +0530

    added some elixir code for testing

--- lib/mpl_bubblegum_nifs.ex
+++ lib/mpl_bubblegum_nifs.ex
@@ -1,6 +1,6 @@
 defmodule MplBubblegumNifs do
   use Rustler, otp_app: :mpl_bubblegum_nifs, crate: "mplbubblegumnifs"
-  @rpc_url "https://api.devnet.solana.com"
+  @rpc_url "https://devnet.helius-rpc.com/?api-key=53da17ee-6973-4f78-ab61-fd7a59f1cc80"
 
   def send_transaction(tx_hash) do
     request_body =
@@ -39,7 +39,18 @@ defmodule MplBubblegumNifs do
     :erlang.nif_error(:nif_not_loaded)
   end
 
-  def transfer_builder(_secret_key, _to_address, _merkle_tree, _index) do
+  def transfer_builder(
+        payer_secret_key,
+        to_address,
+        asset_id,
+        owner,
+        nonce,
+        data_hash,
+        creator_hash,
+        root,
+        proof,
+        merkle_tree
+      ) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
@@ -62,14 +73,84 @@ defmodule MplBubblegumNifs do
     send_transaction(tx_hash)
   end
 
-  def transfer(index) do
-    tx_hash =
+  # MplBubblegumNifs.transfer("DGckRTAtcEe25UGbtuc7xDVUSC67VeFGHNgrVy1G2uAb", "6hxBtjckJxUf9FM8V9dDq1Wux5azG2a64osiNwP1KwDN", "DLgacSweX6fmAbnzwoFnVwcuGRMwFvdCzzwhrXuE5pPc")
+  def transfer(asset_id, to_address, owner) do
+    request_body =
+      %{
+        "id" => "test",
+        "jsonrpc" => "2.0",
+        "method" => "getAssetBatch",
+        "params" => %{
+          "ids" => [asset_id]
+        }
+      }
+      |> Jason.encode!()
+
+    headers = [{"Content-Type", "application/json"}]
+
+    response =
+      case(HTTPoison.post(@rpc_url, request_body, headers)) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body |> Jason.decode!()
+
+        {:ok, %HTTPoison.Response{status_code: status, body: body}} ->
+          IO.puts("Error: HTTP #{status}")
+          IO.inspect(body)
+
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          IO.puts("Request failed:")
+          IO.inspect(reason)
+      end
+
+    asset_proof_request =
+      %{
+        "id" => "test",
+        "jsonrpc" => "2.0",
+        "method" => "getAssetProofBatch",
+        "params" => %{
+          "ids" => [asset_id]
+        }
+      }
+      |> Jason.encode!()
+
+    response2 =
+      case(HTTPoison.post(@rpc_url, asset_proof_request, headers)) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body |> Jason.decode!()
+
+        {:ok, %HTTPoison.Response{status_code: status, body: body}} ->
+          IO.puts("Error: HTTP #{status}")
+          IO.inspect(body)
+
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          IO.puts("Request failed:")
+          IO.inspect(reason)
+      end
+
+    [data1] = response["result"]
+    proof = response2["result"][asset_id]["proof"]
+    root = response2["result"][asset_id]["root"]
+    compression = data1["compression"]
+    creator_hash = compression["creator_hash"]
+    data_hash = compression["data_hash"]
+    nonce = compression["leaf_id"]
+    merkle_tree = compression["tree"]
+
+    tx =
       transfer_builder(
         "UpTcTstVRrUTHQHdxsy84yUTKXp4CCg2dfNP6XVZJ4gUtp4uCCa849rkiWaDHfobtdrxj3KzE8t2zK2tUgrhSdG",
-        "to address",
-        "BB4PeT5Vaorg9V5nqyct3dyyo3RwtZQ61FQR9JK3EnKL",
-        index
+        to_address,
+        asset_id,
+        owner,
+        nonce,
+        data_hash,
+        creator_hash,
+        root,
+        proof,
+        merkle_tree
       )
+
+    tx
   end
 
   def add(_a, _b) do
